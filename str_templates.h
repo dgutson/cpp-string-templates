73fb99fc9b48892af30b3b8e76a78aeb84ad241e
Daniel Gutson <daniel@eclypsium.com>
2020-05-27 08:33:41 -0300

    Add include barriers.

--- str_templates.h
+++ str_templates.h
@@ -21,6 +21,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
+#ifndef STR_TEMPLATES_H
+#define STR_TEMPLATES_H
+
 #include <string>
 #include <list>
 
@@ -169,4 +172,6 @@ public:
         return output;
     }
 };
+
+#endif
 
